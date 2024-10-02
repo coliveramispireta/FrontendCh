aaf23994593a8a16118bc10ef97e7c8caae81de6
coliveramispireta <coliveramispireta@gmail.com>
2024-10-02 00:41:48 -0500

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    reactStrictMode: false,
+    swcMinify: true,
+    images: {
+      domains: ['localhost'],
+    },
+  };
 
 export default nextConfig;
